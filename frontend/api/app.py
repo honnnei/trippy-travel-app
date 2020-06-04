068a0c70877d73642440fe0c13d810652ac0472a
honnnei <hannpop@gmail.com>
2020-06-04 13:01:00 +0200

    create user post request

--- frontend/api/app.py
+++ frontend/api/app.py
@@ -1,8 +1,99 @@
-import time
-from flask import Flask
+from flask import (Flask, render_template, url_for, request, redirect)
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy_utils import EmailType
+from sqlalchemy import ForeignKey
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///trippy.db'
+db = SQLAlchemy(app)
 
-@app.route('/time')
-def get_current_time():
-    return {'time': time.time()}
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(EmailType, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+    display_name = db.Column(db.String(200), nullable=False)
+    bio = db.Column(db.String(255), default="Hi, I'm new to Trippy!")
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    Trip = db.relationship('Trip', backref='User', lazy=True)
+
+    def __repr__(self):
+        return '<User %r>' % self.id
+
+class Trip(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, ForeignKey('user.id'))
+    trip_country = db.Column(db.String(200), nullable=False)
+    trip_bio = db.Column(db.String(200), nullable=True)
+    trip_length = db.Column(db.Integer)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Trip %r>' % self.id
+
+class Feed(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    trip_id = db.Column(db.Integer, ForeignKey('trip.id'))
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Feed %r>' % self.id
+
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    if request.method == 'GET':
+        return "Hello World"
+    elif request.method == 'POST':
+        user_email = request.form['email']
+        user_password = request.form['password']
+        user_display_name = request.form['display_name']
+        new_user = User(email = user_email, password = user_password, display_name = user_display_name)
+
+        try:
+            db.session.add(new_user)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue adding user'
+    else:
+        return 'Post did not work'
+
+# @app.route('/delete/<int:id>')
+# def delete(id):
+#     task_to_delete = Todo.query.get_or_404(id)
+
+#     try:
+#         db.session.delete(task_to_delete)
+#         db.session.commit()
+#         return redirect('/')
+#     except:
+#         return 'There was a problem deleting that task'
+
+# @app.route('/update/<int:id>', methods=['GET', 'POST'])
+# def update(id):
+#     task = Todo.query.get_or_404(id)
+#     if request.method == 'POST':
+#         task.content = request.form['content']
+        
+#         try:
+#             db.session.commit()
+#             return redirect('/')
+#         except:
+#             return 'There was an issue updating your task'
+            
+#     else:
+#         return render_template('update.html', task=task)
+
+
+#     try:
+#         db.session.put(task_to_update)
+#         db.session.commit()
+#         return redirect('/')
+#     except:
+#         return 'There was a problem deleting that task'
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
