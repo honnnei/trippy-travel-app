d0360a5d90294a0dfed07c3d1a2198b5887d37c1
honnnei <hannpop@gmail.com>
2020-06-10 18:03:56 +0200

    User Profile fixed, no followers

--- frontend/src/Containers/UserProfile.js
+++ frontend/src/Containers/UserProfile.js
@@ -22,110 +22,65 @@ function UserProfile() {
   const [mapShow, setMapShow] = useState(true);
   const [timelineShow, setTimelineShow] = useState(false);
   const [galleryShow, setGalleryShow] = useState(false);
-  const [followersShow, setFollowersShow] = useState(false);
+  const [userId, setUserId] = useState(jwt_decode(localStorage.usertoken).identity.user_id);
 
   if(!localStorage.usertoken){
     window.location.href="/"
   }
-
-  const [userId, setUserId] = useState(jwt_decode(localStorage.usertoken).identity.user_id);
- 
   
-
-
   const toggleMap = () => {
     setMapShow(true)
     setTimelineShow(false)
     setGalleryShow(false)
     setFollowersShow(false)
   }
+
   const toggleTimeline = () => {
     setTimelineShow(true)
     setMapShow(false)
     setGalleryShow(false)
     setFollowersShow(false)
   }
+
   const toggleGallery = () => {
     setGalleryShow(true)
     setTimelineShow(false)
     setMapShow(false)
     setFollowersShow(false)
   }
-  const toggleFollowers = () => {
-    setFollowersShow(true)
-    setTimelineShow(false)
-    setGalleryShow(false)
-    setMapShow(false)
-  }
-  // const getUserId = () => {
-  //     // console.log(localStorage.usertoken);
-  //     const token = localStorage.usertoken;
-  //     const decoded = jwt_decode(token);
-  //     setUserId(decoded.identity.user_id)
-  //     console.log(decoded.identity.user_id)
-  // }
-  
-  
-  // useEffect(() => {
-  //   getUserId();
-  //   }, []);
 
-    return (
-      <div>
-        <NavbarComponent />
-                  
-        
-        <Container>
-          <Row className="justify-content-md-center">
-            <Col md={2}>
-              <UserInfo />
-            </Col>
-            <Col md={{ span: 9, offset: 1 }}>
-             <div className="timeline-gallery-map-container">
-                <Nav justify variant="tabs" defaultActiveKey="maps">
-                    <Nav.Item>
-                      <Nav.Link eventKey="maps" onClick={toggleMap}> Map </Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                      <Nav.Link eventKey="timeline" onClick={toggleTimeline}> Timeline </Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                      <Nav.Link eventKey="gallery" onClick={toggleGallery}> Gallery </Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                      <Nav.Link eventKey="followers" onClick={toggleFollowers} > Followers </Nav.Link>
-                    </Nav.Item>
-                  </Nav>
-                  <div className="t-g-m-container">
-                    {timelineShow ? <Timeline /> : ""}
-                    {galleryShow ? <Gallery /> : ""}
-                    {mapShow ? <UserMap /> : ""}
-                    {followersShow ? <Followers /> : ""}
-
-                  </div>
+  return (
+    <div>
+      <NavbarComponent />
+      <Container>
+        <Row className="justify-content-md-center">
+          <Col md={2}>
+            <UserInfo />
+          </Col>
+          <Col md={{ span: 9, offset: 1 }}>
+            <div className="timeline-gallery-map-container">
+              <Nav justify variant="tabs" defaultActiveKey="maps">
+                <Nav.Item>
+                  <Nav.Link eventKey="maps" onClick={toggleMap}> Map </Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="timeline" onClick={toggleTimeline}> Timeline </Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="gallery" onClick={toggleGallery}> Gallery </Nav.Link>
+                </Nav.Item>
+              </Nav>
+              <div className="t-g-m-container">
+                {timelineShow ? <Timeline /> : ""}
+                {galleryShow ? <Gallery /> : ""}
+                {mapShow ? <UserMap /> : ""}
               </div>
-            </Col>
-                      
-          </Row>
-        </Container>
-            {/* <div className="modal">
-              <Modal isOpen={modal} toggle={toggleAddTripModal}> 
-                <ModalHeader toggle={toggleAddTripModal}>Create a Trip:</ModalHeader>
-                <ModalBody>
-                    <AddTripForm />
-                </ModalBody>
-                <ModalFooter>
-                  <Button className="modalBtn" onClick={() => {toggleAddTripModal();}}>Create</Button>
-                  <Button className="modalBtn2" onClick={toggleAddTripModal} id="cancel" >Cancel</Button>
-                </ModalFooter>
-              </Modal>
             </div>
-           */}
-        
-      
-     </div>
-
-    );
+          </Col>        
+        </Row>
+      </Container>
+    </div>
+  );
 }
 
 export default UserProfile;
